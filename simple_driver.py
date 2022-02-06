6c76c43d3e3c923a9db1277b36b6ffd1c37a072c
ronal11 <72041669+ronal11@users.noreply.github.com>
2022-02-06 14:07:54 -0800

    initial commit

--- simple_driver.py
+++ simple_driver.py
@@ -1,15 +1,15 @@
-# Import libraries
+
 import RPi.GPIO as GPIO
 import time
 
 # Set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 
-# Set pin 11 as an output, and set servo1 as pin 11 as PWM
+# Set pin 3 as an output, and set servo1 as pin 3 as PWM
 GPIO.setup(3,GPIO.OUT)
-servo1 = GPIO.PWM(3,50) # Note 11 is pin, 50 = 50Hz pulse
+servo1 = GPIO.PWM(3,50) 
 
-#start PWM running, but with value of 0 (pulse off)
+
 servo1.start(0)
 print ("Waiting for 2 seconds")
 time.sleep(2)
