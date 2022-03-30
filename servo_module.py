2f3a5ee782d121a051c9b792e40f9bf56ecf6656
ronal11 <72041669+ronal11@users.noreply.github.com>
2022-03-30 13:25:30 -0700

    added function parameter for delay

--- servo_module.py
+++ servo_module.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
+MAX_RANGE = 270
+MIN_RANGE = 0
+
 class servo:
     def __init__(self, pin, freq, servoNum):
         self.pin = pin
@@ -11,21 +14,16 @@ class servo:
         self.servoNum.start(0)
         self.previous_angle = 0
 
-    def set_angle(self, angle):
+    def set_angle(self, angle, delay=0.5):
         if self.previous_angle == angle:
             print("desired angle same as previous")
-        elif angle > 270:
+        elif angle > MAX_RANGE:
             print("desired angle greater than 270")
-        elif angle < 0:
+        elif angle < MIN_RANGE:
             print("desired angle less than 0")
         else:
             self.servoNum.ChangeDutyCycle(2+(angle/27))
             print(2+(angle/27))
-            time.sleep(0.5)
+            time.sleep(delay)
             self.servoNum.ChangeDutyCycle(0)
             self.previous_angle = angle
-        
-        
-        
-        
-        
