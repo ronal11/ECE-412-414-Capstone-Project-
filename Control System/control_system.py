bef0b354170567c321d5246530af8f3a8abe0ed0
ronal11 <72041669+ronal11@users.noreply.github.com>
2022-06-09 18:36:13 -0700

    removed detection for loop
    fixed bug that spins motor when solenoid opens or closes

--- Control System/control_system.py
+++ Control System/control_system.py
@@ -16,7 +16,6 @@ import cv_test
 COCO_PATH           = 'coco.names'
 CUSTOM_ClASSES_PATH = 'obj.names'
 SYS_WEBCAM = 0
-frame_thresh = 5
 
 step_pin = 3                          # pin assignments 
 direc_pin = 5
@@ -24,8 +23,8 @@ servo_pin = 7
 button_pin = 35
 solenoid_pin = 37
 irrig_pin = 11
-delay = .003 #.009                   # delay to control motor speed
-verbose_sweep = False                 # variables for debugging 
+delay = .01                   # delay to control motor speed
+verbose_sweep = True                 # variables for debugging 
 verbose_track = True
 sleep_pin = 33
 
@@ -48,10 +47,9 @@ cw = True
 solenoid_open = False
 Pest = False
 x_cord = 0
-irrigation = False
+irrigation =  False
 GPIO.output(sleep_pin, GPIO.HIGH)
 time.sleep(.2)
-GPIO.output(solenoid_pin, GPIO.LOW)
 GPIO.output(solenoid_pin, GPIO.HIGH)
 GPIO.output(sleep_pin, GPIO.LOW)
 # start CV
@@ -60,8 +58,8 @@ cap = cv.toggle_camera(None, SYS_WEBCAM)
 time.sleep(5)
 
 while True:
-    if GPIO.event_detected(button_pin):                      # if falling edge is detected from button
-       return_to_init_pos(motor1, delay, verbose_sweep)     # return to 0 position
+    #if GPIO.event_detected(button_pin):                      # if falling edge is detected from button
+       #return_to_init_pos(motor1, delay, verbose_sweep)     # return to 0 position
     '''
     if GPIO.event_detected(irrig_pin):
         if irrigation:
@@ -91,15 +89,6 @@ while True:
                 pest = False
                 print("no pest detetcted")
        
-        '''
-        key = cv2.waitKey(1)
-        
-        if key == 27:
-            break
-        elif key == 116:  # t -> Toggle the camera
-            cap = cv.toggle_camera(cap, SYS_WEBCAM)
-    
-        '''
     
     
         if pest == True:                         
@@ -110,58 +99,38 @@ while True:
                 track_pest(motor1, delay, verbose_track, x_cord)
             
                 if not solenoid_open:       # if solenoid isn't already opened, then we open it
-                    GPIO.output(sleep_pin, GPIO.LOW)
-                    GPIO.output(solenoid_pin, GPIO.HIGH)
-                    solenoid_open = True
                     GPIO.output(sleep_pin, GPIO.HIGH)
+                    GPIO.output(solenoid_pin, GPIO.LOW)
+                    solenoid_open = True
+                    GPIO.output(sleep_pin, GPIO.LOW)
                 '''                       
                 needed_angle = servo_angle_needed(125)             #code for servo 
             
                 if(servo1.previous_angle == needed_angle):
                     if not solenoid_open:
-                        GPIO.output(solenoid_pin, GPIO.HIGH)
+                        GPIO.output(solenoid_pin, GPIO.LOW)
                         solenoid_open = True
             
                 else:
                     #servo1.set_angle(needed_angle)
                     if not solenoid_open:
-                        GPIO.output(solenoid_pin, GPIO.HIGH)
+                        GPIO.output(solenoid_pin, GPIO.LOW)
                         solenoid_open = True
                 
                 '''
-                #time.sleep(1.5)
-                count = 0
-                for i in range(0, frame_thresh):               # check if pest is still in frame
-                    if cap:
-                        #print("in cap")
-                        center_coords = cv.process_frame_for_coords(cap, net, class_names)
-                    
-                        count = count + 1
-                        if center_coords[0] != None:
-                            print("Center Coords: (x,y) -> (" + str(center_coords[0]) + ", " + str(center_coords[1]) + ")")
-                            x_cord = center_coords[0]
-                            print("x_cord: ", x_cord)
-                            print("count: ", count)
-                            pest = True                    # if pest in detected give new x cordinate 
-                        
-                            # print("Detected Object Class: " + str(class_names[class_ids[0]]) + " | Confidence: " + str(round(confidences[0], 2)))
-                        
-                            break                          # break out of for loop to track pest 
-                        else:
-                            pest = False                   # else set pest to false to indicate no pest detected 
-                            print("no pest detetcted")
-                    print("count: ", count)
-            
+                time.sleep(3.5)
+                pest = False
+                
                 if pest == True:                           # if pest is detected continue in pest deterring loop
                     print("pest still in frame")
                     continue
                 
                 else:                                      # otherwise exit out of loop and continue sweeping
-                    GPIO.output(sleep_pin, GPIO.LOW)
-                    GPIO.output(solenoid_pin, GPIO.LOW)    # close solenoid
+                    GPIO.output(sleep_pin, GPIO.HIGH)
+                    GPIO.output(solenoid_pin, GPIO.HIGH)    # close solenoid
                     print("pest not in frame anymore (presumably)")
                     solenoid_open = False
-                    GPIO.output(sleep_pin, GPIO.HIGH)
+                    GPIO.output(sleep_pin, GPIO.LOW)
                     break                         
     
     cw = motor_sweep(motor1, delay, verbose_sweep, cw) # this is the normal sweeping/irrigation motion while no pest is detected 
